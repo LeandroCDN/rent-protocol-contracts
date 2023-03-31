3cbf88a2770c639e88aa59dfb90b988218503edf
PatoVerde <leandrodenos@gmail.com>
2023-03-31 16:56:58 -0300

    end principal functions in contract, need test all

--- contracts/Rent.sol
+++ contracts/Rent.sol
@@ -10,8 +10,8 @@ contract Rent is ERC721URIStorage, Ownable{
   using Counters for Counters.Counter;
   
   enum states {Free,Ocuped,Stoped}
+  
   struct PropertyData{
-    address owner;
     address renter;
     states state;
     uint reserve; //in ether
@@ -21,6 +21,15 @@ contract Rent is ERC721URIStorage, Ownable{
     uint32 maxTimeToRent; // in seconds
     uint32 startRentDate; // in block.timestamp
     uint32 pricePerSecond; // (price/ 30 days) Can be changed, if there is a specific conflict resolution route;
+    uint32 endRentDate;
+  }
+
+  struct tickets{
+    address from;
+    uint16 startTicketDate;
+    string tikectUri;
+    address lastResult;
+    uint32 lastAmountResult;
   }
 
   uint public totalRegisters;
@@ -32,6 +41,7 @@ contract Rent is ERC721URIStorage, Ownable{
   mapping(address => uint[] id) public ownersOfpropertyList;
   mapping(uint id => uint rentAmount) public amountToPayRents;
   mapping(uint id => uint lastClaim) public lastClaims;
+  mapping(uint id => tickets)public ticketdata;
   
   event Register(PropertyData newRegister, uint id);
   event NewRent(uint id, address renter, uint cantOfMonths);
@@ -60,36 +70,79 @@ contract Rent is ERC721URIStorage, Ownable{
       property.minTimeToRent >= cantOfMonths  && cantOfMonths < property.maxTimeToRent, 
       "The property  must be in Free mode"
     );
-
+    // TODO : LOS FEES PA
     uint totalAmount = (property.reserve + cantOfMonths ) *  property.price;
     coin.transferFrom(msg.sender, address(this), totalAmount);
-    property.startRentDate = uint32(block.timestamp);
     amountToPayRents[id] += property.price * cantOfMonths;
+    property.startRentDate = uint32(block.timestamp);
     property.state = states.Ocuped;
+    property.renter = msg.sender;
+    property.endRentDate = uint32(block.timestamp + (cantOfMonths * (30 days) ));
     emit NewRent(id, msg.sender, cantOfMonths);
   }
 
   function claimPayment(uint id) public {
+    uint toPay = calculatePayment(id);
+    require(amountToPayRents[id] >= toPay ,"Not founds in rents");
+    require(registerPropertyData[id].state == states.Ocuped);
+    require(coin.balanceOf(address(this)) >= toPay ,"Not founds in contract");
+    amountToPayRents[id] -= toPay;
+    coin.transfer(ownerOf(id), toPay);
+    lastClaims[id] = block.timestamp;
+
+    //TODO reset state if finish.
 
   }
 
-  function createTicket() public {
+  function createTicket(uint id, tickets memory _ticketData) public {
+    // stop payments
+    // estate solucion entre las dos partes x tiempo(24hrs)
+    // entidad 
+    PropertyData storage property = registerPropertyData[id];
+    require( 
+      msg.sender == ownerOf(id) || msg.sender == property.renter, 
+      "solo las partes involucradas puede crear un tiket" 
+    );
+    property.state= states.Stoped;
+    _ticketData.startTicketDate = uint16(block.timestamp);
+    ticketdata[id] = _ticketData;
 
   }
 
-  function resolveTicket(uint id) public {
+  function resolveTicket(uint id, address winer, uint amount) public {
+    tickets storage ticket = ticketdata[id];
+    PropertyData storage property = registerPropertyData[id];
+    //uint timeSinceTicket = uint16(block.timestamp) - ticket.startTicketDate;
 
+    if(msg.sender == owner()){
+      ticket.lastResult = winer;
+      ticket.lastAmountResult = uint32(amount);
+      require(amount >= (property.reserve + amountToPayRents[id]), "resolve amount");
+      if(amount > property.reserve){
+        uint newAmount = amount-property.reserve;
+        property.reserve = 0;
+        amountToPayRents[id] -=  newAmount;
+        //todo recaulculate pricePerScond (amountToPayRents / (endRentDate-lastClaim) )
+        property.pricePerSecond = uint32(amountToPayRents[id] / (uint(property.endRentDate) - lastClaims[id])); 
+        coin.transfer(winer, amount);
+      }else{
+        property.reserve -= amount;
+        coin.transfer(winer, amount);
+      }
+      property.state= states.Free;
+    }
   }
 
   // view functions
   function viewData(uint id) public view returns(PropertyData memory){
-
+    return registerPropertyData[id];
   }
 
   function calculatePayment(uint id) public view returns(uint){
     PropertyData memory property = registerPropertyData[id];
     require(uint(property.state) == 1);
     //This function mustbe calculate how be deliver to ownerOfProperty
+    //TODO if the property no esta alquilada
     uint timeSinceLastClaim = (block.timestamp - lastClaims[id]);
     uint rentToPay = timeSinceLastClaim * uint(property.pricePerSecond);
    
